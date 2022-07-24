c356893212cbb9d7c87989b81a26d8a2aca5c40d
Seunghwan Cho <seunghwan.cho@edu.sait.ca>
2022-07-24 15:19:26 -0600

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.scss";
+
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Software Developer
+          </a>
+          <div className="itemContainer">
+          
+            <span>780 902 5713</span>
+          </div>
+          <div className="itemContainer">
+            
+            <span>Seunghwan Cho</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
